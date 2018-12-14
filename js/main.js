626d41dbe73294ba8c6dd3889522404d09c914a9
acatalas <acatala@iesfbmoll.org>
2018-12-14 16:14:28 +0100

    Tryng to fix Git CDN erorr

--- js/main.js
+++ js/main.js
@@ -1,24 +1,30 @@
 
 $(document).ready(function () {
-    $('.day').eq(new Date().getDay()-1).parent().addClass('text-warning');
     $("#submit").click(function () {
-        $(":text, textarea").each(function() {
+        var empty = false;
+        //Checks if any of the inputs is empty
+        $('#form').find('input, textarea').each(function() {
+            console.log($(this).val());
             if($(this).val() === ""){
-                Swal({
-                    type: 'error',
-                    title: 'Emplena tots els camps del formulari',
-                    confirmButtonColor: '#28A745',
-                })
-            } else {
-                Swal({
-                    type: 'success',
-                    title: 'Correu enviat exitosament!',
-                    showConfirmButton: false,
-                    timer: 2000
-                })
+                empty = true;
             }
-         });
-        
+        });
+        if(empty){
+            //Show error
+            Swal({
+                type: 'error',
+                title: 'Emplena tots els camps del formulari',
+                confirmButtonColor: '#28A745',
+            })
+        } else {
+            //Show success
+            Swal({
+                type: 'success',
+                title: 'Correu enviat exitosament!',
+                showConfirmButton: false,
+                timer: 2000
+            })    
+        }
     });
     $("#clear").click(function () {
         Swal({
@@ -35,6 +41,13 @@ $(document).ready(function () {
             }
         })
     });
+    var curDay = new Date().getDay();
+    //If Sunday
+    if (curDay === 0){
+        $('.day').eq(6).parent().addClass('text-warning');
+    } else {
+        $('.day').eq(curDay-1).parent().addClass('text-warning');
+    }
     
 
 });
